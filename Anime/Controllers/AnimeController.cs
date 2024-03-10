a1dc839979bb5d9ed1ca33322cfbc94e8e88f092
SergioRaposo <87394233+SergioRaposo20@users.noreply.github.com>
2024-03-10 18:50:55 -0300

    Adicionar arquivos de projeto.

---
+++ Anime/Controllers/AnimeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnimeController : ControllerBase
+    {
+        [HttpGet(Name = "teste")]
+        public IEnumerable<string> GetAll()
+        {
+            return "Olá mundo";
+        }
+    }
+}
